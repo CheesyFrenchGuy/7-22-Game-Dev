2c6074863cb1e04345564e313b335bf367cefe67
CheesyFrenchGuy <thatcheesyfrenchguy@gmail.com>
2020-07-23 17:09:03 +0000

    fUNCTIONS FOR ALL SHAPES

--- main.py
+++ main.py
@@ -18,16 +18,31 @@ def triangle(length, color):
     x+=1
   bob.end_fill()
 
+def Circle(radius, color):
+  bob.fillcolor(color)
+  bob.begin_fill()
+  bob.circle(int(radius))
+  bob.end_fill()
+
+def Star(length, color):
+  bob.fillcolor(color)
+  bob.begin_fill()
+  x=0
+  while x<5:
+    bob.forward(int(length))
+    bob.right(144)
+    x+=1
+  bob.end_fill()
+  
+
 
 input_shape = input("What shape do you want to draw?: ")
 input_length = input("Choose how big: ")
 input_color = input("What color do you want?: ")
 
 if input_shape == "Triangle":
-  print("Triangle")
+  triangle(input_length, input_color)
 elif input_shape == "Circle":
-  print("Circle")
+  Circle(input_length,input_color)
 elif input_shape == "Star":
-  print("Star")
-
-triangle(input_length, input_color)
+  Star(input_length, input_color)
